ac7c87b3958bd36e9b843a39ea16ecd6dfff7508
Keith Langan <keithlangangti@gmail.com>
2018-10-06 20:51:42 +0100

    jQuery examples

---
+++ jQueryEffects.js
@@ -0,0 +1,5 @@
+$("button").on("click", function(){
+$('div').fadeToggle(2000, function(){
+	//$(this).remove();
+});
+});
---
+++ jQueryExcercise.js
@@ -0,0 +1,8 @@
+//select all divs and give them a colored background
+$("div").css("background", "purple");
+//select divs with class highlight and make them 200px bigger
+$(".highlight").css("width", "200px");
+//select the div with the id "third" and give it an orange border
+$("#third").css("border", "2px solid orange");
+//select the first div only and make its font color pink
+$("div:first-of-type").css("color", "pink");
